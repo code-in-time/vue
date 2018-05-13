3d5aff1e52e1aad0412bb133e42ec62e3efb5a56
code-in-time <andrew.crawford.za@gmail.com>
2018-05-13 06:10:13 +0200

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,16 @@
+https://www.youtube.com/watch?v=nyJSd6V2DRI&list=PL55RiY5tL51p-YU-Uw90qQH419BM4Iz07
+
+<div id="app">
+  {{title}}
+</div>
+
+
+
+
+new Vue({
+  el: "#app",
+  data: {
+		title: 'hello'
+    }
+});
+
